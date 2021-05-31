dec1256566bb3a4d79639542375dfe2dfbcc54eb
william <william16544@gmail.com>
2021-05-31 22:43:55 +0800

    modify 創件帳號畫面 連動選單製作

--- app/src/main/java/com/test/firebasetast/createNewAccountActivity.java
+++ app/src/main/java/com/test/firebasetast/createNewAccountActivity.java
@@ -4,24 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class createNewAccountActivity extends AppCompatActivity {
 
-    Spinner address;
-    EditText addressSearch;
+    Spinner address,subAddress;
     EditText account,password;
     Button create_newAccount,create_back;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,10 +52,14 @@ public class createNewAccountActivity extends AppCompatActivity {
     public void init(){
         account = findViewById(R.id.account);
         password = findViewById(R.id.password);
+
         address = findViewById(R.id.address);
+        subAddress = findViewById(R.id.subAddress);
 //        addressSearch = findViewById(R.id.addressSearch);
+
         create_newAccount = findViewById(R.id.create_newAccount);
         create_back = findViewById(R.id.create_back);
+
 
 
     }
@@ -97,17 +113,65 @@ public class createNewAccountActivity extends AppCompatActivity {
 
     //設定 下拉選單 選項
     public void setSpinnerData(){
-//        List<String> addressList = new ArrayList<>();
-//        addressList.add("中和區");
-//        addressList.add("永");
-//        addressList.add("和");
-//        addressList.add("區");
-//        addressList.add("和區");
-//        ArrayAdapter arrayAdapter = new ArrayAdapter(this,
-//                android.R.layout.simple_dropdown_item_1line,addressList);
-        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(this,R.array.address_array,
-                android.R.layout.simple_expandable_list_item_1);
-        address.setAdapter(arrayAdapter);
+        String TAG = "setSpinnerData";
+        FirebaseDatabase fbd = FirebaseDatabase.getInstance();
+        DatabaseReference db = fbd.getReference();
+
+
+        db.child("location").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            //跟ui同線程
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "onComplete: is successful");
+                    HashMap<String, ArrayList> sublocation = (HashMap) task.getResult().getValue();
+
+                    //主下拉選單
+                    ArrayList<String> location = new ArrayList<>();
+                    for(String sublocationKey : sublocation.keySet()){
+                        location.add(sublocationKey);
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),
+                                    android.R.layout.simple_dropdown_item_1line,location);
+                            address.setAdapter(arrayAdapter);
+
+                            ArrayAdapter arrayAdapter2 = new ArrayAdapter(getApplicationContext(),
+                                    android.R.layout.simple_dropdown_item_1line,sublocation.get(location.get(0)).toArray());
+                            subAddress.setAdapter(arrayAdapter2);
+
+
+                            address.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                                    int position = address.getSelectedItemPosition();
+                                    ArrayAdapter arrayAdapter2 = new ArrayAdapter(getApplicationContext(),
+                                            android.R.layout.simple_dropdown_item_1line,sublocation.get(location.get(position)).toArray());
+                                    subAddress.setAdapter(arrayAdapter2);
+                                }
+                                @Override
+                                public void onNothingSelected(AdapterView<?> adapterView) {
+                                    Log.d(TAG, "onNothingSelected: error ");
+                                }
+                            });
+                        }
+                    });
+                }else {
+                    Log.d(TAG, "onComplete: is not successful");
+                }
+            }
+        });
+
+
+
+
+//        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(this,R.array.address_array,
+//                android.R.layout.simple_expandable_list_item_1);
+//        address.setAdapter(arrayAdapter);
     }
 
 
